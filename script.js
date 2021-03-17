d7ba12a76bdae673320f3abb5324ab64948e5e66
6erik <estanton@csumb.edu>
2021-03-16 21:43:04 -0700

    Added option to use button or enter key to start the game

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
+const buttonElement = document.getElementById('startButton')
+
+
+let timerOn = false
 
 quoteInputElement.addEventListener('input', () => {
+  
   const arrayQuote = quoteDisplayElement.querySelectorAll('span')
   const arrayValue = quoteInputElement.value.split('')
 
   let correct = true
   arrayQuote.forEach((characterSpan, index) => {
     const character = arrayValue[index]
+
     if (character == null) {
       characterSpan.classList.remove('correct')
       characterSpan.classList.remove('incorrect')
@@ -28,49 +33,52 @@ quoteInputElement.addEventListener('input', () => {
   if (correct) stopTimer()
 })
 
-function getRandomQuote() {
-  return fetch(RANDOM_QUOTE_API_URL)
-    .then(response => response.json())
-    .then(data => data.content)
-}
-
-async function renderNewQuote1() {
-  const quote = await getRandomQuote()
-  quoteDisplayElement.innerHTML = ''
-  quote.split('').forEach(character => {
-    const characterSpan = document.createElement('span')
-    characterSpan.innerText = character
-    quoteDisplayElement.appendChild(characterSpan)
-  })
-  quoteInputElement.value = null
+buttonElement.addEventListener("click", () => {
+  console.log("start button pressed - timer started")
   startTimer()
-}
+});
+
+body.addEventListener('keyup',function(e){
+  if (!timerOn) {
+    if (e.keyCode === 13) {
+      console.log("enter key pressed - timer started")
+      startTimer()
+    }
+  }
+});
 
 async function renderNewQuote() {
   const quote = "public final class HelloWorld {\n" + 
                 "public static void main(String[] args) {\n" +
-                "System.out.println(\"Hello, JavaPoet!\")\;\n" +
+                "System.out.println(\"Hello, World!\")\;\n" +
                 "}\n" +
                 "}";
   quoteDisplayElement.innerHTML = ''
   quote.split('').forEach(character => {
     const characterSpan = document.createElement('span')
-    characterSpan.innerText = character
+    if (character == "\t") {
+      //character = "&#160"
+      characterSpan.innerHTML = character
+    } else {
+      characterSpan.innerText = character
+    }
     quoteDisplayElement.appendChild(characterSpan)
   })
   quoteInputElement.value = null
-  startTimer()
 }
 
 
 let startTime
-var counterInterval = setInterval(() => {
-  timer.innerText = getTimerTime()
-}, 1000)
+let counterInterval
+
 function startTimer() {
+  counterInterval = setInterval(() => {
+    timerElement.innerText = getTimerTime()
+  }, 1000)
+
+  timerOn = true
   timerElement.innerText = 0
   startTime = new Date()
-  
 }
 
 function getTimerTime() {
@@ -78,7 +86,9 @@ function getTimerTime() {
 }
 
 function stopTimer() {
-  clearInterval(counterInterval)
+  timerOn = false
+  clearInterval(counterInterval);
+  timerElement.innerText = getTimerTime();
 }
 
 renderNewQuote()
