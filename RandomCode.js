720b5ed5a9ee85f83d7d7468e95220f6e3549de7
6erik <estanton@csumb.edu>
2021-04-21 20:49:53 -0700

    Updated css to accomodate new code block sizes

--- RandomCode.js
+++ RandomCode.js
@@ -1,35 +1,32 @@
 class RandomCode {
     constructor() {
-        // line 1
-        this.l1_1 = "public class ";
-        this.l1_2arr = ["HelloWorld ", "HelloCountry ", "TestProgram ", "TypingTest ", "GarbageClass "];
-        
-        // line 2
-        this.l2_1 = "public static void main(String[] args) {"
-
-        // section 3
-        this.l3_1 = "int i = 0;\n";
-        this.l3_2 = "while (i < 5) {\n";
-        this.l3_3 = "System.out.println(i);\n";
-        this.l3_4 = "i++;\n";
-
-        // misc characters
-        this.lcb = "{";
-        this.rcb = "}";
-        this.nl = "\n";
     }
 
     generateCodeBlock() {
         var codeBlock;
-        let l1_2= this.l1_2arr[Math.floor(Math.random() * this.l1_2arr.length)];
-        let l1 = this.l1_1 + l1_2 + this.lcb + this.nl;
-        let l2 = this.l2_1 + this.nl;
-        let l3 = this.l3_1 + this.l3_2 + this.l3_3 + this.l3_4 + this.rcb + this.nl;
-        let l4 = this.rcb + this.nl;
-        let l5 = this.rcb;
+        let topBlock = this.genTopBlock();
+        let innerBlock = this.genInnerBlock();
+        let bottomBlock = "}\n}";
 
-
-        codeBlock = l1 + l2 + l3 + l4 + l5;
+        codeBlock = topBlock + innerBlock + bottomBlock;
         return codeBlock;
+    }
+    genTopBlock() {
+        var topBlock;
+        let topBlockArr1 = ["HelloWorld ", "HelloCountry ", "TestProgram ", "TypingTest ", "GarbageClass ", "FakeApplication "];
+        
+        topBlock = "public class " + topBlockArr1[Math.floor(Math.random() * topBlockArr1.length)] + "{\npublic static void main(String[] args) {\n";
+        return topBlock;
+    }
+
+    genInnerBlock() {
+        var innerBlock;
+        let innerBlockArr = [];
+        innerBlockArr.push("for (int i = 0; i < 5; i++) {\nSystem.out.println(i);\n}\n"); // for loop
+        innerBlockArr.push("int i = 0;\nwhile (i < 5) {\nSystem.out.println(i);\ni++;\n}\n"); // while loop
+        innerBlockArr.push('int x = 20;\nint y = 18;\nif (x > y) {\nSystem.out.println("x is greater than y");\n}\n'); // if statement
+
+        innerBlock = innerBlockArr[Math.floor(Math.random() * innerBlockArr.length)];
+        return innerBlock;
     }
 }
